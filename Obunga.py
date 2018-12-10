8fb77f88e49b7d2e2c9d26fe9a998022ca6b29be
YoteYiteYate <45661904+YoteYiteYate@users.noreply.github.com>
2018-12-10 08:08:01 -0600

    Add files via upload

---
+++ Obunga.py
@@ -0,0 +1,28 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+white = (255, 255, 255)
+
+bat_y = 4
+
+def draw_bat():
+    sense.set_pixel(0, bat_y, white)
+    sense.set_pixel(0, bat_y + 1, white)
+    sense.set_pixel(0, bat_y - 1, white)
+
+
+draw_bat()
+    
+
+def move_up(event):
+    global bat_y
+    if event.action == 'pressed' and bat_y > 1:
+        bat_y -= 1
+
+def move_down(event):
+    global bat_y
+    if event.action == 'pressed' and bat_y < 6:
+        bat_y += 1
+
+sense.stick.direction_up = move_up
+sense.stick.direction_down = move_down
